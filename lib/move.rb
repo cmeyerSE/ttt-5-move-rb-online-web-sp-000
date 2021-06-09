8561e2d674462419c023ef80ff3d54317a9cfc24
Cory Meyer <cameyer182@gmail.com>
2021-06-09 23:49:16 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -5,3 +5,9 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+
+def input_to_index
+  puts "user_input.to_i"
+  user_input = "1" - "9"
+  converted_input = input_to_index(user_input)
+end
